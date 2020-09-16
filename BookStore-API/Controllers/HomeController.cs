6fa375687590cdbea52a65361f3b8278c5a6141f
Mike Kobrin <mikekobrin@gmail.com>
2020-09-16 16:20:13 +0200

    Added Data objects

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test API controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accesed Home Controller");
+            return new string[] { "Value1", "value2" };
+        }
+
+        /// <summary>
+        /// Gets a value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Home/5
+        [HttpGet("{id}", Name ="Get")]
+        public string Get(int id)
+        {
+            _logger.LogDebug($"Got value {id}");
+            return "value";
+        }
+        
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("Logging an Error");
+        }
+        
+        // POST: api/Home/5
+        [HttpPost("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        } 
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("Warning for the Delete method");
+        }
+    }
+}
